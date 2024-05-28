ea8aeac0219ff9e00bcb9d2a09baa87fde1e1e56
Akalanka99 <akalankakulathilaka@gmail.com>
2024-05-28 13:02:21 +0530

    first commit

---
+++ mern-server/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 5000;
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+//mongodp configeration
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mern-book-store:jJRpF4A1snnt0jkk@cluster0.kvpobt3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //create a collection of documents
+    const bookcollection = client.db("bookInventory").collection("books");
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
