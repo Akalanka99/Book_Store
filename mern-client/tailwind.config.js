ce51ef9b8303dfa1f70f6015189a96a7e20d8e88
Akalanka99 <akalankakulathilaka@gmail.com>
2024-06-05 16:01:31 +0530

    updated

---
+++ mern-client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+    // Add paths to all of the templates you're using.
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
