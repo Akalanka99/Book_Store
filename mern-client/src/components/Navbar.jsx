52f21e9503785365e9cf5b086e429a8e9f1e0407
Akalanka99 <akalankakulathilaka@gailcom>
2024-07-31 21:01:34 +0530

    Swiper_side

--- mern-client/src/components/Navbar.jsx
+++ mern-client/src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 //react icon
-import { FaBlog } from "react-icons/fa6";
+import { FaBarsStaggered, FaBlog, FaXmark } from "react-icons/fa6";
 
 
 const Navbar = () => {
     const[isMenuOpen, setIsMenuOpen] = useState(false);
     const[isSticky, setIsSticky] = useState(false);
+   
+   
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     }
@@ -23,12 +25,12 @@ const Navbar = () => {
 
         window.addEventListener('scroll', handleScroll);
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            window.addEventListener('scroll', handleScroll);
         }
     }, []);
 
    // naviteam 
-   const naviteam = [
+   const navIteams = [
     {link:"Home", path:"/"},
     {link:"About", path:"/about"},
     {link:"Shop", path:"/shop"},
@@ -36,19 +38,37 @@ const Navbar = () => {
     {link:"Blog", path:"/blog"},
    ]
   return (
-    <header>
-        <nav>
-            <div>
+    <header className='w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300'>
+        <nav className={'py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""}'}>
+            <div className='flex justify-between items-center text-base gap-8' >
                 {/* logo */}
                 <Link to = "/"className='text-2xl font-bold text-blue-700 flex items-center gap-2'><FaBlog className='inline-block'/>Books</Link>
 
                 {/* naviteam */}
-                <ul>
+                <ul className=' md:flex space-x-12 hidden'>
                     {
-                      naviteam.map(({link, path}) => <Link key={path} to={path} className='block text-base text-black uppercase cursor-pointer hover:text-blue-700'>{link}</Link>)  
+                      navIteams.map(({link, path}) => <Link key={path} to={path} className='block text-base text-black uppercase cursor-pointer hover:text-blue-700'>{link}</Link>)  
                     }
                 </ul>
+                {/* button for log devices */}
+                <div className='space-x-12 hidden lg:flex items-center '>
+                    <button><FaBarsStaggered className='w-5 hover:text-blue-700'/></button>
+                </div>
+                {/* menu button mobile devices */}
+                <div className='md:hidden'>
+                    <button onClick={toggleMenu} className='text-black focus:outline-none '>
+                        {
+                            isMenuOpen ? <FaXmark className='h-5 w-5 text-black'/> : <FaBarsStaggered className='h -5 w-5 text-black'/>
+                        }
+                    </button>
+
+                </div>
             </div>
+
+          
+           
+           
+           
         </nav>  
     </header>
   )
